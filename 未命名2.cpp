4d0e9f85ac8ac223f143d5117ee9799ca98d039f
jeffernson <630711047@qq.com>
2017-10-16 08:54:57 +0800

    students'system

---
+++ 未命名2.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std; 
+int main()
+{
+ cout<<"\t\t ．．．．．．．欢迎光临．．．．．．．．\n";
+ cout<<"\t\t       │学生信息管理系统│\n";
+ cout<<"\t\t        =================\n";
+ cout<<"\t\t │ 1. 添加记录    2. 显示记录 │\n";
+ cout<<"\t\t │                            │\n";
+ cout<<"\t\t │ 3. 信息查询    4. 数据计算 │\n";
+ cout<<"\t\t │                            │\n";
+ cout<<"\t\t │ 5. 修改记录    6. 删除记录 │\n";
+ cout<<"\t\t │                            │\n";
+ cout<<"\t\t │ 0. 退出程序                │\n";
+ cout<<"\t\t   ─────────────—  \n";
+ cout<<"\t\t请输入0-6";
+ return 0;
+}
